994ce97f49f288989e538f4012a0f7b91d88aba4
ck-nuu <github@mdanso.com>
2025-09-22 07:29:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Change this to your GitHub repo name
+const repoName = "portfolio-web-apps";
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${repoName}/`, // ensures correct paths on GitHub Pages
+});
