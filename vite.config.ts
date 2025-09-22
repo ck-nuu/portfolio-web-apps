3e966dd1e21f8a53be5205b63c3c2046705bc276
Chorkor Nuu <64353660+ck-nuu@users.noreply.github.com>
2025-09-22 07:22:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/portfolio-web-apps/",  // Important: your repo name here
+  resolve: {
+    alias: {
+      "@/components": "/src/components"
+    }
+  }
+});
