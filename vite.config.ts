8c539ae3b0cbb62d48b73eac3525714fde95f6c9
ck-nuu <github@mdanso.com>
2025-09-22 07:46:16 +0100

    Monday after meeting with Mr. Kudjoe

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-<<<<<<< HEAD
-
-// Change this to your GitHub repo name
-const repoName = "portfolio-web-apps";
-
-export default defineConfig({
-  plugins: [react()],
-  base: `/${repoName}/`, // ensures correct paths on GitHub Pages
-=======
-
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio-web-apps/",  // Important: your repo name here
@@ -18,5 +8,4 @@ export default defineConfig({
       "@/components": "/src/components"
     }
   }
->>>>>>> 3e966dd1e21f8a53be5205b63c3c2046705bc276
 });
